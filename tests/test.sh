29d68d1f6972ef40646a96e37a0d9e7ada24c362
cody <cjosephp15@gmail.com>
2023-05-19 15:38:49 +1000

    Fixed compare issues with test script

--- tests/test.sh
+++ tests/test.sh
@@ -1,3 +1,7 @@
+NC='\033[0m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
 
 test()
 {
@@ -7,12 +11,20 @@ test()
         fi
 
         for i in $(ls cases/$grade/$color/tests); do
+            echo "${NC}__________________________________________________"
+            echo "Testing $grade/$color/$i\n"
             ./../bin/Debug/net7.0/CAB201 $color cases/$grade/$color/tests/$i cases/$grade/$color/output/$i
-            cmp cases/$grade/$color/output/$i cases/$grade/$color/answers/$i
+
+            if [ ! -f cases/$grade/$color/output/$i ]; then
+                echo "${YELLOW}error"
+                continue
+            fi
+
+            diff -b -s cases/$grade/$color/output/$i cases/$grade/$color/answers/$i
             if [ $? -eq 1 ]; then
-                echo "false"
+                echo "${RED}false"
             else
-                echo "true"
+                echo "${GREEN}true"
             fi
         done
     done
