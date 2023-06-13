d29b77b79155456f228aa8aed24c6c2c56193e96
cody <cjosephp15@gmail.com>
2023-06-13 20:50:15 +1000

    Setup testing

---
+++ Tests/UnitTests.cs
@@ -0,0 +1,15 @@
+﻿using Engine;
+
+namespace UnitTests;
+
+[TestClass]
+public class FunctionalityTests
+{
+    [TestMethod]
+    public void InitialBoardIO()
+    {
+        string fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+        Board board = new Board(fen);
+        Assert.AreEqual(Board.ToFEN(board), fen);
+    }
+}
