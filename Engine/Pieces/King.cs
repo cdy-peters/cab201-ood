e8077cda13483b7054e532d312b1f0cdeee4522f
cody <cjosephp15@gmail.com>
2023-06-14 15:49:33 +1000

    Added castling move

--- Engine/Pieces/King.cs
+++ Engine/Pieces/King.cs
@@ -31,6 +31,8 @@ namespace Engine
 
                     AddMove(board, square, destPos);
                 }
+
+            Castle(board);
         }
 
         /// <summary>
@@ -73,8 +75,52 @@ namespace Engine
 
             // If destination piece is general, set check
             Moves.IsGeneralInCheck(board, destPos);
-            
+
             square.Piece.ValidMoves.Add(new MoveDest(destPos, false));
+        }
+
+        private static void Castle(Board board)
+        {
+            if (board.Player == PieceColor.White)
+            {
+                Piece king = board.Squares[60].Piece;
+                if (king == null)
+                    return;
+
+                if (board.WhiteCastleKingSide)
+                {
+                    if (board.Squares[61].Piece == null && board.Squares[62].Piece == null)
+                        if (!board.ThreatenedByBlack[61] && !board.ThreatenedByBlack[62])
+                            king.ValidMoves.Add(new MoveDest(62, true));
+                }
+
+                if (board.WhiteCastleQueenSide)
+                {
+                    if (board.Squares[61].Piece == null && board.Squares[62].Piece == null && board.Squares[63].Piece == null)
+                        if (!board.ThreatenedByBlack[58] && !board.ThreatenedByBlack[59])
+                            king.ValidMoves.Add(new MoveDest(58, true));
+                }
+            }
+            else
+            {
+                Piece king = board.Squares[4].Piece;
+                if (king == null)
+                    return;
+
+                if (board.BlackCastleKingSide)
+                {
+                    if (board.Squares[5].Piece == null && board.Squares[6].Piece == null)
+                        if (!board.ThreatenedByWhite[5] && !board.ThreatenedByWhite[6])
+                            king.ValidMoves.Add(new MoveDest(6, true));
+                }
+
+                if (board.BlackCastleQueenSide)
+                {
+                    if (board.Squares[1].Piece == null && board.Squares[2].Piece == null && board.Squares[3].Piece == null)
+                        if (!board.ThreatenedByWhite[2] && !board.ThreatenedByWhite[3])
+                            king.ValidMoves.Add(new MoveDest(2, true));
+                }
+            }
         }
     }
 }
